1c38d0c3f368cd268446fa3b37f1d4379f15d62a
xiangchao <xiangchao@cares.sh.cn>
2018-09-12 16:08:27 +0800

    add something

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public static void main(String[]args){
         System.out.println("Hello World !")
         System.out.println("this is dev change !")
+        System.out.println("why!")
         }
 
